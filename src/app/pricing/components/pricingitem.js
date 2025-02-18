e5b6a7e4fd13ff9df6bd61cf69d366a69a9c0ddd
Rique-Borges <henriqueribeiroborges@gmail.com>
2025-02-18 15:33:53 -0300

    add: responsividade na página de preços

--- src/app/pricing/components/pricingitem.js
+++ src/app/pricing/components/pricingitem.js
@@ -1,9 +1,12 @@
 const PricingItem = ({ name, price, description, isHighlighted }) => {
   return (
-    <li className={`p-4 rounded-md transition-all duration-500 ease-in-out ${isHighlighted ? 'bg-[#8ED1FB] text-white' : 'bg-gray-100 text-gray-700'}`}>
-      <h3 className="font-semibold text-lg">{name}</h3>
-      <p className="text-sm">{description}</p>
-      <p className="text-lg font-bold">{price}</p>
+    <li
+      className={`p-4 rounded-md transition-all duration-500 ease-in-out 
+        ${isHighlighted ? 'bg-[#8ED1FB] text-white' : 'bg-gray-100 text-gray-700'}`}
+    >
+      <h3 className="font-semibold text-lg md:text-xl">{name}</h3>
+      <p className="text-sm md:text-base">{description}</p>
+      <p className="text-lg font-bold md:text-xl">{price}</p>
     </li>
   );
 };
--- src/app/pricing/page.js
+++ src/app/pricing/page.js
@@ -39,9 +39,9 @@ const Page = () => {
   const selectedCategory = searchParams.get('category') || null;
 
   return (
-    <div className="max-w-3xl mx-auto p-6">
-      <h1 className="text-5xl font-bold text-center text-gray-700 mb-6">Pricing</h1>
-      <div className="space-y-6 mx-16">
+    <div className="max-w-3xl mx-auto p-4 md:p-6">
+      <h1 className="text-4xl md:text-5xl font-bold text-center text-gray-700 mb-6">Pricing</h1>
+      <div className="space-y-6 mx-4 md:mx-16">
         {pricingData.map((section, index) => (
           <PricingCategory
             key={index}
@@ -57,3 +57,4 @@ const Page = () => {
 };
 
 export default Page;
+
